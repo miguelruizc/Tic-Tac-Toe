704f7e8a0d8c30132834bebcdb29ac7488011310
miguelruizc <buumchakalaka@gmail.com>
2024-05-06 18:14:37 +0200

    Add Cell object, that represents a cell on the board

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello world!")
+//GameController
+//Gameboard
+
+//Cell
+function Cell() {
+    // Cell: Represents a cell that holds a value, '0' by default
+    let value = '0';
+
+    const setValue = (newValue) => {
+        value = newValue;
+    };
+
+    const getValue = () => value;
+
+    return {
+        setValue,
+        getValue
+    };
+}
+//Player
+
+let c = Cell();
+console.log(c.getValue());
+c.setValue("X");
+console.log(c.getValue());
